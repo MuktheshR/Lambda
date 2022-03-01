5326ab4302155e9df32f903626762d48d4c3b00f
MuktheshR <98938244+MuktheshR@users.noreply.github.com>
2022-03-01 21:01:38 +0530

    Lambda Assignment

---
+++ Lambdaq4/src/Lambdaq4.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Lambdaq4 {
+
+	public static void main(String[] args) {
+		
+	   ArrayList<String> list1 = new ArrayList();
+	   list1.add("bhargav");
+	   list1.add("Vishnu");
+	   list1.add("Rohan");
+	   list1.add("Mukthesh");
+	   list1.add("RamShaym");
+	   list1.removeIf(n -> n.length()%3==0);
+        System.out.println(list1);
+	}
+
+}
